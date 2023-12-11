e9dde769b7fd5faf7ebb168e4d1e3075e7fbd146
Md-Shajib <shajib0607@gmail.com>
2023-12-11 20:40:03 +0600

    Props

--- src/App.js
+++ src/App.js
@@ -16,15 +16,35 @@ function App() {
     name: "Md Roni",
     age: 23
   }
+  const nayoks = [{name: 'Saruk Khan', title: 'Star'}, {name:'Amir Khan', title: 'star'}, {name:'Salman Khan', title:'star'}]
   return (
     <div className="App">
       <header className="App-header">
         <h2>This is first app</h2>
         <h3 style={textStyle}>Name: {person1.name + ", Age: " + person1.age}</h3>
         <h3 style={textStyle}>Name: {person2.name + ", Age: " + person2.age}</h3>
+        <Person name="Shakib Khan" title="Nayok"></Person>
+        <Person name = {nayoks[0].name} title = {nayoks[0].title}></Person>
       </header>
     </div>
   );
 }
 
+
+function Person(props){
+  var style = {
+    border: '2px solid green',
+    color: 'yellow',
+    padding: 10,
+    marginBottom: 3,
+  }
+  return(
+    <div>
+      <div  style={style}>
+        <h2>Name: {props.name}</h2>
+        <h4>Title: {props.title}</h4>
+      </div>
+    </div>
+  );
+}
 export default App;
