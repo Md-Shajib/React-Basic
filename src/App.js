81f7b827de2fc27fd6c1a32c6f4961b729cd12fa
Md-Shajib <shajib0607@gmail.com>
2023-12-11 19:04:53 +0600

    Dynamic HTML in js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var textStyle = {
+    color: 'red',
+    fontSize: 20,
+    padding: 10,
+    margin: 2,
+    backgroundColor: 'gray'
+  }
+  var person1 = {
+    name: "Md Shajib",
+    age: 24
+  }
+  var person2 = {
+    name: "Md Roni",
+    age: 23
+  }
   return (
     <div className="App">
       <header className="App-header">
         <h2>This is first app</h2>
+        <h3 style={textStyle}>Name: {person1.name + ", Age: " + person1.age}</h3>
+        <h3 style={textStyle}>Name: {person2.name + ", Age: " + person2.age}</h3>
       </header>
     </div>
   );
